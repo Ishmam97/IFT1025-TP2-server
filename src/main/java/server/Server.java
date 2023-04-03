0fde56bf4ca99174d4db7664a218d9374f0a5f8d
Ishmam Solaiman <ishmam.a.solaiman@gmail.com>
2023-04-03 04:45:23 -0500

    add required imports for handleLoadCourses

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -2,13 +2,13 @@ package server;
 
 import javafx.util.Pair;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Arrays;
+import server.models.RegistrationForm;
 
 public class Server {
 
