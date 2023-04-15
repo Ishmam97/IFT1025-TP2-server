fb305ce999b4ab6bc00bf8c389759c46c8e07a1a
Ishmam Solaiman <ishmam.a.solaiman@gmail.com>
2023-04-15 18:37:47 -0500

    initialized FXClient Launcher

--- src/main/java/FXClient/FXClient.java
+++ src/main/java/FXClient/FXClient.java
@@ -4,14 +4,6 @@
  */
 package FXClient;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
 /**
  *
  * @author wang
